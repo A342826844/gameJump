087ebe275c7c6400641049ca299c9c957746f1ca
神秘人 <aaa@g.com>
2023-03-15 16:58:01 +0800

    feat: face book sdk

--- android/app/src/main/java/com/trade24/gamejump/MainActivity.java
+++ android/app/src/main/java/com/trade24/gamejump/MainActivity.java
@@ -1,4 +1,4 @@
-package com.gamejump;
+package com.trade24.gamejump;
 
 import android.os.Build;
 import android.os.Bundle;
--- android/app/src/main/java/com/trade24/gamejump/MainApplication.java
+++ android/app/src/main/java/com/trade24/gamejump/MainApplication.java
@@ -1,4 +1,4 @@
-package com.gamejump;
+package com.trade24.gamejump;
 
 import android.app.Application;
 import android.content.res.Configuration;
